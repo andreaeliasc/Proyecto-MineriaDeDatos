7b8a8718e0b4da6d191143afd3a5baaa5114e0ce
Andrea Elias <42878138+andreaeliasc@users.noreply.github.com>
2021-05-24 15:07:27 -0600

    Arbol de decision y anexos

--- finalProject.R
+++ finalProject.R
@@ -102,3 +102,267 @@ nnet_test$grupo_nnet<-columnaMasAlta #Se le a?ade al grupo de prueba el valor de
 
 cfm_nnet <- confusionMatrix(table((factor(nnet_test$grupo, levels=min(nnet_test$grupo):max(nnet_test$grupo))),
                                   (factor(nnet_test$grupo_nnet, levels=min(nnet_test$grupo):max(nnet_test$grupo)))))
+                      
+                      
+
+                      
+                      
+ #Arbol de Decision
+
+datosFiltertree <- dataFinal[,c("EDADHOM","EDADMUJ","DEPREG","MUPREG","PUEMUJ","MESREG","AÑOREG","PUEHOM","ESCHOM","ESCMUJ","CIUOHOM", "CIUOMUJ","DIAOCU","edad_dif","grupo")]
+
+datosFiltertree
+porciento <- 70/100
+
+porciento <- 70/100
+
+
+set.seed(321)
+trainRowsNumber<-sample(1:nrow(datosFiltertree),porciento*nrow(datosFiltertree))
+train<-datosFiltertree[trainRowsNumber,]
+test<-datosFiltertree[-trainRowsNumber,]
+
+dt_model<-rpart(train$grupo~PUEHOM+PUEMUJ+ESCMUJ+ESCHOM,train,method = "class",control =rpart.control(minsplit =1,minbucket=1, cp=0))
+rpart.plot(dt_model, box.palette = "Blues")
+
+prediccion <- predict(dt_model, newdata = test[1:15])
+columnaMasAlta<-apply(prediccion, 1, function(x) colnames(prediccion)[which.max(x)])
+test$prediccion<-columnaMasAlta #Se le a?ade al grupo de prueba el valor de la predicci?n
+cfm<-confusionMatrix(as.factor(test$prediccion),as.factor(test$grupo))
+cfm
+
+
+
+#Anexos
+
+#frecuencia de edad
+freqEdad<-table(dataFinal$edad_dif)
+view(freqEdad)
+barplot(head(freqEdad,15))
+head(freqEdad,15)
+freqEdad <- sort(freqEdad,                   # Vector atómico
+                       decreasing = TRUE, # Ordenar en orden creciente (FALSE) o decrecienete (TRUE)
+                       na.last = TRUE,) 
+freqEdad<-(head(freqEdad,15))
+barplot(freqEdad,15, col = distinctColorPalette(18), main = "Top 15 diferencias de edad", xlab = "diferencia de edad", ylab="frecuencia")
+
+
+
+#Escolaridades mas comunes en diferencias de edades mayores
+
+hey <- table(dataFinal$grupo, dataFinal$ESCMUJ)
+hey<- filter(dataFinal, dataFinal$grupo>=3)
+holi <- table(hey$ESCMUJ)
+
+barplot(holi, col = distinctColorPalette(18), main = "Escolaridad Mujeres", xlab = "Escolaridad", ylab="frecuencia")#,  names.arg =c("Ninguno","Primaria","Basicos","Diversificado","Universitario","Ignorado"))
+
+
+hom <- table(dataFinal$grupo, dataFinal$ESCHOM)
+hom<- filter(dataFinal, dataFinal$grupo>=3)
+homesc <- table(hom$ESCHOM)
+
+barplot(homesc, col = distinctColorPalette(18), main = "Escolaridad Hombres", xlab = "Escolaridad", ylab="frecuencia")#, names.arg =c("Ninguno","Primaria","Basicos","Diversificado","Universitario","Posgrado","Ignorado"))
+
+
+#diferencia de edades medias
+
+muj <- table(dataFinal$grupo, dataFinal$ESCMUJ)
+muj<- filter(dataFinal, dataFinal$grupo==2)
+mujer <- table(muj$ESCMUJ)
+
+barplot(mujer, col = distinctColorPalette(18), main = "Escolaridad Mujeres", xlab = "Escolaridad", ylab="frecuencia",  names.arg =c("Ninguno","Primaria","Basicos","Diversificado","Universitario","Posgrado","Ignorado"))
+
+
+hom <- table(dataFinal$grupo, dataFinal$ESCHOM)
+hom<- filter(dataFinal, dataFinal$grupo==2)
+homesc <- table(hom$ESCHOM)
+
+barplot(homesc, col = distinctColorPalette(18), main = "Escolaridad Hombres", xlab = "Escolaridad", ylab="frecuencia", names.arg =c("Ninguno","Primaria","Basicos","Diversificado","Universitario","Posgrado","Ignorado"))
+
+
+#diferencia de edades menores
+
+muj <- table(dataFinal$grupo, dataFinal$ESCMUJ)
+muj<- filter(dataFinal, dataFinal$grupo>=1)
+mujer <- table(muj$ESCMUJ)
+
+barplot(mujer, col = distinctColorPalette(99), main = "Escolaridad Mujeres", xlab = "Escolaridad", ylab="frecuencia" , names.arg =c("Ninguno","Primaria","Basicos","Diversificado","Universitario","Posgrado","Ignorado"))
+
+
+hom <- table(dataFinal$grupo, dataFinal$ESCHOM)
+hom<- filter(dataFinal, dataFinal$grupo>=1)
+homesc <- table(hom$ESCHOM)
+
+barplot(homesc, col = distinctColorPalette(99), main = "Escolaridad Hombres", xlab = "Escolaridad", ylab="frecuencia", names.arg =c("Ninguno","Primaria","Basicos","Diversificado","Universitario","Posgrado","Ignorado"))
+
+
+
+
+#Pueblos menor dif
+
+muj <- table(dataFinal$grupo, dataFinal$PUEMUJ)
+muj<- filter(dataFinal, dataFinal$grupo>=1)
+mujer <- table(muj$PUEMUJ)
+
+barplot(mujer, col = distinctColorPalette(99), main = "Pueblo de pertenencia de la Mujer", xlab = "Pueblo", ylab="frecuencia", names.arg =c("Maya","Garifuna","Xinca","Mestizo/Ladino","Otro","Ignorado"))
+
+
+hom <- table(dataFinal$grupo, dataFinal$PUEHOM)
+hom<- filter(dataFinal, dataFinal$grupo>=1)
+homesc <- table(hom$PUEHOM)
+
+barplot(homesc, col = distinctColorPalette(99), main = "Pueblo de pertenencia del hombre", xlab = "Pueblo", ylab="frecuencia", names.arg =c("Maya","Garifuna","Xinca","Mestizo/Ladino","Otro","Ignorado"))
+
+#Pueblos dif media
+
+muj <- table(dataFinal$grupo, dataFinal$PUEMUJ)
+muj<- filter(dataFinal, dataFinal$grupo>=2)
+mujer <- table(muj$PUEMUJ)
+
+barplot(mujer, col = distinctColorPalette(99), main = "Pueblo de pertenencia de la Mujer", xlab = "Pueblo", ylab="frecuencia", names.arg =c("Maya","Garifuna","Xinca","Mestizo/Ladino","Otro","Ignorado"))
+
+
+hom <- table(dataFinal$grupo, dataFinal$PUEHOM)
+hom<- filter(dataFinal, dataFinal$grupo>=2)
+homesc <- table(hom$PUEHOM)
+
+barplot(homesc, col = distinctColorPalette(99), main = "Pueblo de pertenencia del hombre", xlab = "Pueblo", ylab="frecuencia", names.arg =c("Maya","Garifuna","Xinca","Mestizo/Ladino","Otro","Ignorado"))
+
+
+
+
+#Pueblos dif alta
+
+muj <- table(dataFinal$grupo, dataFinal$PUEMUJ)
+muj<- filter(dataFinal, dataFinal$grupo>=3)
+mujer <- table(muj$PUEMUJ)
+
+barplot(mujer, col = distinctColorPalette(99), main = "Pueblo de pertenencia de la Mujer", xlab = "Pueblo", ylab="frecuencia", names.arg =c("Maya","Garifuna","Xinca","Mestizo/Ladino","Otro","Ignorado"))
+
+
+hom <- table(dataFinal$grupo, dataFinal$PUEHOM)
+hom<- filter(dataFinal, dataFinal$grupo>=3)
+homesc <- table(hom$PUEHOM)
+
+barplot(homesc, col = distinctColorPalette(99), main = "Pueblo de pertenencia del hombre", xlab = "Pueblo", ylab="frecuencia", names.arg =c("Maya","Garifuna","Xinca","Mestizo/Ladino","Otro","Ignorado"))
+
+
+
+#Municipios dif alta
+
+muj <- table(dataFinal$grupo, dataFinal$MUPREG)
+muj<- filter(dataFinal, dataFinal$grupo>=3)
+muj <-table(muj$MUPREG)
+muji <- sort(muj,                   # Vector atómico
+                 decreasing = TRUE, # Ordenar en orden creciente (FALSE) o decrecienete (TRUE)
+                 na.last = TRUE,) 
+view(muji)
+muji <- (head(muji,10))
+
+barplot(muji, col = distinctColorPalette(99), main = "Municipios de Registro", xlab = "Municipio", ylab="frecuencia",names.arg =c("Guatemala","Jalapa","Villa Nueva","Pto. Barrios","Morales","Coban","Escuintla", "Huehuetenango", "Jutiapa","San Pedro Carcha" ))
+
+
+# dif  media
+muj <- table(dataFinal$grupo, dataFinal$MUPREG)
+muj<- filter(dataFinal, dataFinal$grupo==2)
+muj <-table(muj$MUPREG)
+muji <- sort(muj,                   # Vector atómico
+             decreasing = TRUE, # Ordenar en orden creciente (FALSE) o decrecienete (TRUE)
+             na.last = TRUE,) 
+view(muji)
+muji <- (head(muji,10))
+
+barplot(muji, col = distinctColorPalette(99), main = "Municipios de Registro", xlab = "Municipio", ylab="frecuencia",names.arg =c("Guatemala","Villa Nueva","Coban","San Pedro Carcha","Quetzaltenango","Huehuetenango","Pto. Barrios", "Jalapa", "Jutiapa","Escuintla" ))
+
+#dif baja
+muj <- table(dataFinal$grupo, dataFinal$MUPREG)
+muj<- filter(dataFinal, dataFinal$grupo>=1)
+muj <-table(muj$MUPREG)
+muji <- sort(muj,                   # Vector atómico
+             decreasing = TRUE, # Ordenar en orden creciente (FALSE) o decrecienete (TRUE)
+             na.last = TRUE,) 
+view(muji)
+muji <- (head(muji,10))
+
+barplot(muji, col = distinctColorPalette(99), main = "Municipios de Registro", xlab = "Municipio", ylab="frecuencia",names.arg =c("Guatemala","Villa Nueva","Quetzaltenango","Coban","San Juan Sac","San Pedro Carcha","Totonicapan","Huehuetenango", "Jalapa", "Chimaltenango"))
+
+
+
+
+#Ocupaciones Mujeres dif Baja
+
+# dif  baja
+muj <- table(dataFinal$grupo, dataFinal$CIUOMUJ)
+muj<- filter(dataFinal, dataFinal$grupo==1)
+muj <-table(muj$CIUOMUJ)
+muji <- sort(muj,                   # Vector atómico
+             decreasing = TRUE, # Ordenar en orden creciente (FALSE) o decrecienete (TRUE)
+             na.last = TRUE,) 
+view(muji)
+muji <- (head(muji,5))
+
+barplot(muji, col = distinctColorPalette(99), main = "Ocupaciones Mujer", xlab = "Ocupaciones", ylab="frecuencia",names.arg =c("Servicios Personales", "No registrado", "Enseñanza", "Oficinistas","Contabilidad" ))
+
+
+
+muj <- table(dataFinal$grupo, dataFinal$CIUOHOM)
+muj<- filter(dataFinal, dataFinal$grupo==1)
+muj <-table(muj$CIUOHOM)
+muji <- sort(muj,                   # Vector atómico
+             decreasing = TRUE, # Ordenar en orden creciente (FALSE) o decrecienete (TRUE)
+             na.last = TRUE,) 
+view(muji)
+muji <- (head(muji,5))
+
+barplot(muji, col = distinctColorPalette(99), main = "Ocupaciones Hombre", xlab = "Ocupaciones", ylab="frecuencia",names.arg =c("Peones", "Agricultor", "Vendedor", "Servicios Personales","Construccion" ))
+
+
+muj <- table(dataFinal$grupo, dataFinal$CIUOHOM)
+muj<- filter(dataFinal, dataFinal$grupo==2)
+muj <-table(muj$CIUOHOM)
+muji <- sort(muj,                   # Vector atómico
+             decreasing = TRUE, # Ordenar en orden creciente (FALSE) o decrecienete (TRUE)
+             na.last = TRUE,) 
+view(muji)
+muji <- (head(muji,5))
+
+barplot(muji, col = distinctColorPalette(99), main = "Ocupaciones Hombre", xlab = "Ocupaciones", ylab="frecuencia")#,names.arg =c("Peones", "Servicios Personales", "Agricultor", "Vendedor","Construccion" ))
+
+
+muj <- table(dataFinal$grupo, dataFinal$CIUOMUJ)
+muj<- filter(dataFinal, dataFinal$grupo==2)
+muj <-table(muj$CIUOMUJ)
+muji <- sort(muj,                   # Vector atómico
+             decreasing = TRUE, # Ordenar en orden creciente (FALSE) o decrecienete (TRUE)
+             na.last = TRUE,) 
+view(muji)
+muji <- (head(muji,5))
+
+barplot(muji, col = distinctColorPalette(99), main = "Ocupaciones Mujer", xlab = "Ocupaciones", ylab="frecuencia",names.arg =c("Servicios Personales", "No Registrados", "Enseñanza", "Vendedor","Oficinista" ))
+
+
+
+
+muj <- table(dataFinal$grupo, dataFinal$CIUOHOM)
+muj<- filter(dataFinal, dataFinal$grupo==3)
+muj <-table(muj$CIUOHOM)
+muji <- sort(muj,                   # Vector atómico
+             decreasing = TRUE, # Ordenar en orden creciente (FALSE) o decrecienete (TRUE)
+             na.last = TRUE,) 
+view(muji)
+muji <- (head(muji,5))
+
+barplot(muji, col = distinctColorPalette(99), main = "Ocupaciones Hombre", xlab = "Ocupaciones", ylab="frecuencia",names.arg =c("Peones", "Agricultor", "Vendedor", "Servicios Personales","Construccion" ))
+
+
+muj <- table(dataFinal$grupo, dataFinal$CIUOMUJ)
+muj<- filter(dataFinal, dataFinal$grupo==3)
+muj <-table(muj$CIUOMUJ)
+muji <- sort(muj,                   # Vector atómico
+             decreasing = TRUE, # Ordenar en orden creciente (FALSE) o decrecienete (TRUE)
+             na.last = TRUE,) 
+view(muji)
+muji <- (head(muji,5))
+
+barplot(muji, col = distinctColorPalette(99), main = "Ocupaciones Mujer", xlab = "Ocupaciones", ylab="frecuencia" ,names.arg =c("Servicios Personales", "No Registrados", "Vendedor", "Enseñanza","Oficinista" ))
+
